b271a51b756d40a0d37bfd26c636d23831e99cef
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2024-09-16 22:25:23 +0200

    ♻️ Revised Mediator

---
+++ BehavioralDesignPatterns/Mediator/Employees/Cook.cs
@@ -0,0 +1,11 @@
+﻿namespace Mediator.Employees
+{
+    public class Cook : Employee
+    {
+        public Cook(string name) : base(name)
+        {
+        }
+
+        public override string ToString() => $"Cook {Name}";
+    }
+}
