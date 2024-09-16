adb6b67ac0a75430141a8dd6b80ce081b6e3bd45
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2024-09-16 22:15:38 +0200

    ♻️ Revised Facade

---
+++ StructuralDesignPatterns/Facade/Entities/Drink.cs
@@ -0,0 +1,17 @@
+﻿namespace Facade.Entities
+{
+    public class Drink
+    {
+        public Drink(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString() => $"{Name}, costs {Price}.";
+    }
+}
--- StructuralDesignPatterns/Facade/Program.cs
+++ StructuralDesignPatterns/Facade/Program.cs
@@ -1,11 +1,18 @@
-﻿namespace Facade
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Facade facade = new Facade();
-            facade.Pay();
-        }
-    }
-}
+﻿using Facade.Entities;
+
+Facade.Facade facade = new();
+
+Order order = new
+([
+    new Drink("Tea", price: 2.0),
+    new Drink("Coffee", price: 3.0),
+    new Drink("Hot Chocolate", price: 3.0)
+]);
+
+Console.WriteLine("Your order includes:");
+order.Print();
+
+Console.Write("Enter amount to pay: ");
+double cash = Convert.ToDouble(Console.ReadLine());
+
+facade.PlaceOrder(order, cash);
