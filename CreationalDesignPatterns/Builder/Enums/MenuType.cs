8802b6543c58381f2dfcf70aa889617d141d1a75
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2024-09-16 22:08:15 +0200

    ♻️ Revised Builder

---
+++ CreationalDesignPatterns/Builder/Enums/MenuType.cs
@@ -0,0 +1,10 @@
+﻿namespace Builder.Enums
+{
+    public enum MenuType
+    {
+        HotDrinkAndDessert = 1,
+        ColdDrinkAndDessert = 2,
+        HotDrinkAndColdDrink = 3,
+        HotDrinkAndColdDrinkAndDessert = 4
+    }
+}
