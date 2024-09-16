501be8ca245383d9bbf60a470acf5e81a9cd0a68
Alexandra Valkova <alexandra.valkova.97@gmail.com>
2024-09-16 21:54:53 +0200

    ♻️ Revised Simple Factory

---
+++ CreationalDesignPatterns/SimpleFactory/Coffees/Ristretto.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleFactory.Coffees
+{
+    public class Ristretto : Coffee
+    {
+        public override string Name => "Ristretto";
+
+        public override int CoffeeContent => 20;
+
+        public override int MilkContent => 0;
+    }
+}
